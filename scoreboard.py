cac3f33df423a85bcd8522668b34efde04235a37
green <greenspencer.10@gmail.com>
2024-01-24 11:59:25 -0500

    add scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(-550, -250)
+        self.write("0", align="center", font=("Arial", 24, "normal"))
+
+    def update_scoreboard(self, color_score):
+        self.clear()
+        self.goto(-550, -250)
+        self.score += color_score
+        self.write(f"{self.score}", align="center", font=("Arial", 24, "normal"))
+
+# ...
